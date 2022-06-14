5a415fca1f13f12406116776cbd35d4588442616
jazzdestructor <jazzdestructor@gmail.com>
2022-06-14 22:28:43 +0530

    Completed Navbar section in desktop and mobile view

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { images } from "../../constants";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+
+import "./Navbar.scss";
 
 const Navbar = () => {
-  return <nav></nav>;
+  const [toggle, setToggle] = useState();
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li key={`link-${item}`} className="app__flex p-text">
+            <div />
+            <a href={`/${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeInOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {["home", "about", "work", "skills", "contact"].map((item) => (
+                <li key={`${item}`}>
+                  <a onClick={() => setToggle(false)} href={`/${item}`}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
