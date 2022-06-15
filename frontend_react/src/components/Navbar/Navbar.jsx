b1b37b28e351fa2a7517369d224433818cd1b9a6
jazzdestructor <jazzdestructor@gmail.com>
2022-06-15 22:05:41 +0530

    Navbar fixes and Added Header Changes

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { images } from "../../constants";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 
+import { images } from "../../constants";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState();
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -16,7 +17,7 @@ const Navbar = () => {
         {["home", "about", "work", "skills", "contact"].map((item) => (
           <li key={`link-${item}`} className="app__flex p-text">
             <div />
-            <a href={`/${item}`}>{item}</a>
+            <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
@@ -30,8 +31,8 @@ const Navbar = () => {
             <HiX onClick={() => setToggle(false)} />
             <ul>
               {["home", "about", "work", "skills", "contact"].map((item) => (
-                <li key={`${item}`}>
-                  <a onClick={() => setToggle(false)} href={`/${item}`}>
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
                   </a>
                 </li>
--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
 import "./Header.scss";
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div id="home" className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am </p>
+              <h1 className="head-text">Jay Kapade</h1>
+            </div>
+          </div>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">FullStack Web Developer &nbsp;💻</p>
+            <p className="p-text">UI/UX Designer &nbsp;✏️</p>
+            <p className="p-text">3d Artist and Illustrator &nbsp;🎨</p>
+          </div>
+        </div>
+      </motion.div>
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile-img" />
+      </motion.div>
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.redux, images.javascript, images.sass].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
 };
 
 export default Header;
